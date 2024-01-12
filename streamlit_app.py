e635d3bf7cb65699c0f0ebe44f92f538889e876c
stormywarehouselyfe <dgoodell22@gmail.com>
2024-01-12 09:42:53 -0500

    Update streamlit_app.py
    play2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title('My Parents\' New Healthy Diner')
 st.header('Breakfast Menu', divider='blue')
 st.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+st.markdown('Omega 3 & Blueberry Oatmeal<br>\
+            Kale, Spinach, & Rocket Smoothie<br>\
+            Hard-Boiled Free-Range Egg')
