65903c2d3c1740cade188ef28bad6a572d9298cf
stormywarehouselyfe <dgoodell22@gmail.com>
2024-01-12 09:23:29 -0500

    Update streamlit_app.py
    Experimenting with markdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents\' New Healthy Diner')
+st.title('My Parents\' New Healthy Diner')
+st.header('Breakfast Menu', divider='blue')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
