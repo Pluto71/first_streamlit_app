5dd91789d8ff33d30ed328c24e6e8662688413e8
dgoodell <david.goodell@concordusa.com>
2024-01-12 10:00:09 -0500

    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,6 @@ import streamlit as st
 
 st.title('My Parents\' New Healthy Diner')
 st.header('Breakfast Menu', divider='blue')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-menu = '''Omega 3 & Blueberry Oatmeal
-
-Kale, Spinach, & Rocket Smoothie
-
-Hard-Boiled Free-Range Egg
-'''
-st.markdown(menu)
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach, & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
