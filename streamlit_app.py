cd8eda11bcbabbec583707fcafa793829699e602
stormywarehouselyfe <dgoodell22@gmail.com>
2024-01-12 15:38:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,43 @@
-import streamlit as st
+import streamlit
 import pandas
 import requests
 
-st.title('My Mom\'s New Healthy Diner')
-st.header('Breakfast Menu', divider='blue')
-st.text('🥣 Omega 3 & Blueberry Oatmeal')
-st.text('🥗 Kale, Spinach, & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 Avocado Toast')
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast Menu', divider='blue')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-st.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
-st.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize json response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Format normalized json data into readable dataframe
-st.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
