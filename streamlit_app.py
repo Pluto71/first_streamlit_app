c610fdb9a133988ce60e783f3d05f26444d4a1d8
stormywarehouse2 <dgoodell22@gmail.com>
2024-02-20 23:05:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,9 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('Some Title')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Proverbs 25:2')
+streamlit.text('🌷 It is the glory of God to conceal a matter;')
+streamlit.text('  to search out a matter is the glory of kings. 👑)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
