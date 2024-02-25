5266ff3ad12c5652b2cd005a30393a3969fa901a
stormywarehouse2 <dgoodell22@gmail.com>
2024-02-24 23:16:21 -0500

    Update wreckit.py

--- pages/wreckit.py
+++ pages/wreckit.py
@@ -9,4 +9,14 @@ thoughts = '''
 - Surely, Lot was a sinful man. Sitting in Sodom? Not according to Peter.  
 -- (Test this!)
 '''
+
+light = '''
+1 In the beginning was the Word, and the Word was with God, and the Word was God.  
+2 He was with God in the beginning.  
+3 Through him all things were made; without him nothing was made that has been made.  
+4 In him was life, and that life was the light of all mankind.  
+5 The light shines in the darkness, and the darkness has not overcome it.  
+'''
+
 st.markdown(thoughts)
+st.markdown(light)
