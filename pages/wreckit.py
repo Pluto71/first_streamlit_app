2da96a60ff344a608087bbefb29e61f29933f67a
stormywarehouse2 <dgoodell22@gmail.com>
2024-02-24 21:23:31 -0500

    Update wreckit.py

--- pages/wreckit.py
+++ pages/wreckit.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.markdown("# Wreck It 🪨")
 st.sidebar.markdown("# Wreck It 🏰")
+
+
+st.text('A few questions:')
+st.markdown('- Surely, Lot was a sinful man. Sitting in Sodom? Not according to Pete, or Hebrews')
